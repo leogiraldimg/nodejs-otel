6dead441a58997eb626500d185074e561040c2a7
Leonardo Giraldi Moreno Giuranno <giraldi.dev@outlook.com>
2024-05-16 22:22:47 -0300

    chore: add base project

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["@typescript-eslint"],
+    rules: {
+        indent: [
+            "error",
+            4,
+            {
+                ignoredNodes: ["PropertyDefinition"],
+            },
+        ],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+    },
+};
